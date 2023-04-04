5807be93b3f187dfa7638b7e7573d772685eee36
Ethan Brown <ewbrowntech@gmail.com>
2023-04-04 16:21:46 -0500

    Added comments to block_size.py

--- block_size.py
+++ block_size.py
@@ -10,10 +10,17 @@ import math
 import os
 
 
+# The "block size" is the trigger value used in this Context-"Triggered" Piecewise Hashing algorithm.
+# The initial block size is that which is computed before any of the input is read
 def compute_initial_block_size(filepath):
     filesize = os.stat(filepath).st_size
-    min_block_size = 3
-    ss_length = 64
 
-    initial_block_size = min_block_size * math.pow(2, (math.log2(filesize / (ss_length * min_block_size))))
+    # The minimum block size represents the smallest amount of data that will be hashed at a time by the rolling hash
+    MIN_BLOCK_SIZE = 3
+
+    # The spamsum length represents the length of the signature created by the algorithm. A longer signature conveys
+    # more information about its respective file, but is more computationally expensive to compare against another
+    SS_LENGTH = 64
+
+    initial_block_size = MIN_BLOCK_SIZE * math.pow(2, (math.log2(filesize / (SS_LENGTH * MIN_BLOCK_SIZE))))
     return initial_block_size
